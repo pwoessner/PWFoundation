fd270476a32108a8ed02bebb219342eb466a6b72
Philipp Woessner <philipp.woessner@sap.com>
2023-03-10 12:01:26 +0100

    added Bundle Keys String extension

---
+++ Sources/PWFoundation/Extensions/String+BundleKeys.swift
@@ -0,0 +1,13 @@
+//
+//  String+BundleKeys.swift
+//  
+//
+//  Created by Woessner, Philipp on 10.03.23.
+//
+
+import Foundation
+
+public extension String {
+    static let shortVersion = "CFBundleShortVersionString"
+    static let bundleVersion = "CFBundleVersion"
+}
