5996f7b4685940cc98895d6b82c1feaf92f38c88
Philipp Wössner <64482866+pwoessner@users.noreply.github.com>
2023-03-25 09:51:19 +0100

    Normalize function on [Double] (#2)
    * WIP normalize func
    
    * implemented normalize function on Double

---
+++ Sources/PWFoundation/Extensions/Double+Normalize.swift
@@ -0,0 +1,20 @@
+//
+//  Double+Normalize.swift
+//  
+//
+//  Created by Woessner, Philipp on 24.03.23.
+//
+
+import Foundation
+
+public extension [Double] {
+    func normalized() -> [Double]? {
+        guard let min = self.min(),
+              let max = self.max() else {
+            return nil
+        }
+
+        let diff = max - min
+        return self.map { ($0 - min) / diff }
+    }
+}
