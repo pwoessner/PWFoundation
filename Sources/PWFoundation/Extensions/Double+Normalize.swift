b286558a12dd1bb5b7756af51ea3f28a82bd1c35
Philipp Woessner <philipp.woessner@sap.com>
2023-03-25 08:26:54 +0100

    WIP normalize func

---
+++ Sources/PWFoundation/Extensions/Double+Normalize.swift
@@ -0,0 +1,25 @@
+//
+//  Double+Normalize.swift
+//  
+//
+//  Created by Woessner, Philipp on 24.03.23.
+//
+
+import Foundation
+
+public extension [Double] {
+//    func normalize() -> [Double] {
+//        guard let minWins = map(\.wins).min(),
+//              let maxWins = map(\.wins).max() else {
+//            return
+//        }
+//        let diffWins = maxWins - minWins
+//        let diffLosses = maxLosses - minLosses
+//
+//        for (index, champion) in enumerated() {
+//            let normalizedWins = Double(champion.wins - minWins) / Double(diffWins)
+//            let normalizedLosses = Double(champion.losses - minLosses) / Double(diffLosses)
+//            self[index].rating = winWeight * normalizedWins - lossWeight * normalizedLosses
+//        }
+//    }
+}
