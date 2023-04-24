09e2f12c1e6844e00169152f9aa201c0300eb209
Philipp Wössner <pwoessner17@gmail.com>
2023-04-24 19:30:09 +0200

    Date convenience init for milliseconds

--- Sources/PWFoundation/Extensions/Date+Convenience.swift
+++ Sources/PWFoundation/Extensions/Date+Convenience.swift
@@ -8,6 +8,13 @@
 import Foundation
 
 public extension Date {
+    private static let millisecondsToSecondsRatio = 1000
+
+    public init?(milliseconds: Int?) {
+        guard let milliseconds else { return nil }
+        self = Date(timeIntervalSince1970: TimeInterval(milliseconds / Self.millisecondsToSecondsRatio))
+    }
+
 	func getDate(in number: Int, _ component: Calendar.Component) -> Date? {
 		Calendar.current.date(byAdding: component, value: number, to: self)
 	}
